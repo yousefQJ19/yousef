415142ccd94b55360c32220aa5fa93b273c51f02
yousefQJ19 <92521652+yousefQJ19@users.noreply.github.com>
2021-10-16 23:23:51 +0300

    Create inex.js

---
+++ inex.js
@@ -0,0 +1,46 @@
+/*
+console.log('hellow');
+alert('yooooo');
+//comennt 
+var b = 'smothie';
+console.log(b);
+
+document.getElementById('par').innerHTML = 'hey there a7a';
+//var age = prompt('what is ure age');
+//console.log(age);
+function fun(){
+    var name =prompt('whats youre full name ');
+    var result='hellow' + ' ' + name ;
+    console.log(result);
+}
+fun();
+
+function sum(num1,num2){
+    var result=num1+num2;
+    console.log(result);
+}
+sum(10,10);
+
+var test=0;
+while (test<100){
+        test+=1;
+        console.log(test);
+}
+ 
+for (let num=0;num<=100;num++){
+    console.log(num);
+}
+//let name={first:'yousef',last:'samara'};
+*/
+
+
+let doc='appele,bannana,melon';
+console.log(doc.length);
+console.log(doc.indexOf('app'));
+console.log(doc.slice(0,5));
+console.log(doc.replace('app','dad'));
+console.log(doc.toLowerCase());
+console.log(doc.toUpperCase());
+console.log(doc[2]);
+console.log(doc.split(','))
+console.log(doc.split(''))
